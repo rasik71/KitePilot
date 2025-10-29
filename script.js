f580d510146be6bc2fe8247f5575dcb7f74db6d3
rasik71 <fardin.pc12@gmail.com>
2025-10-30 00:27:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 (() => {
   // ----- Config -----
   const CANVAS_ID = 'gameCanvas';
-  const GRAVITY = 0.25;
+  const GRAVITY = 0.15;
   const FLAP_STRENGTH = -6;
   const PIPE_SPEED = 1.2;
   const PIPE_GAP = 280; // vertical gap
@@ -370,6 +370,7 @@
   });
 
 })();
+
 
 
 
